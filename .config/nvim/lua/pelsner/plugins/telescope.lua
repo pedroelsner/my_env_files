04505a0043ee93ff5ae24a32d8394a15181ebb25
Pedro Elsner <pedro.elsner@gmail.com>
2024-05-11 13:09:38 -0300

    add config to support lsp

--- .config/nvim/lua/pelsner/plugins/telescope.lua
+++ .config/nvim/lua/pelsner/plugins/telescope.lua
@@ -1,14 +1,31 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
-    -- vim.keymap.set('n', '<leader>ps', function() 
-    --   builtin.grep_string({ search = vim.fn.input("Grep > ") });
-    -- end, {})
-  end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
+
+		-- telescope.load_extension("fzf")
+
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+	end,
 }
